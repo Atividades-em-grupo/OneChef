3b25f608b9f6d21e669d3c70708ad01cdc638111
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2021-07-27 10:23:03 -0300

    Criação de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
