8f554318888f70c698da681932e601e9dccd8cbe
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2021-08-17 11:23:42 -0300

    criação de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'quem-somos', component: QuemSomosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
