04905f5027df94e8a49f088c739c359e66e84d13
Gabriel José Soto Ravanhan <gabriel.ravanhan@etec.sp.gov.br>
2021-11-30 23:28:59 -0300

    Continuação dos ajustes

--- src/app/receitas/receita/receita.model.ts
+++ src/app/receitas/receita/receita.model.ts
@@ -2,8 +2,8 @@ export interface Receita {
   id: string;
   nome: string;
   descricao: string;
-  ingrediente: string;
-  preparo: string;
+  ingrediente: string[];
+  preparo: string[];
   avaliacao: number;
   imagem_principal: string;
   imagem_secundaria: string;
